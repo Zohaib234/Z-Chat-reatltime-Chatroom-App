9ceb1df3b8b1c7c8776fd8cc05855a760c55b601
Zohaib234 <21sw031@students.muet.edu.pk>
2022-08-18 17:40:54 +0500

    first version ready

--- public/client.js
+++ public/client.js
@@ -5,3 +5,48 @@ do{
   name = prompt('please enter your name')
 }
 while(!name)
+
+const form = document.getElementById('send-container');
+const inputmessage = document.getElementById('messageInp')
+const chatbox = document.querySelector(".message__area")
+const submit = document.getElementById('btn');
+
+const append = (message,position)=>{
+ 
+  const message_element = document.createElement('div');
+  const classname = position;
+  console.log(classname);
+  message_element.classList.add(classname);
+  message_element.classList.add('message');
+  message_element.innerHTML = `
+  <p>${message}</p>  
+  ` ;
+
+  chatbox.appendChild(message_element);
+}
+
+socket.emit('new-user-joined',name);
+
+socket.on('user-joined',name=>{
+
+  append(`${name} joind the chat`,'incoming');
+
+})
+socket.on('receive', data =>{
+  append(`${data.user}: ${data.message}`,'incoming')
+})
+
+
+// show messages
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  const message = inputmessage.value
+  append(`You : ${message}`,'outgoing');
+  socket.emit('send',message);
+  inputmessage.value='';
+})
+
+
+
+
--- server.js
+++ server.js
@@ -13,11 +13,31 @@ app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'index.html'));
 })
 
+const users = {};
 // establishing connection of socket io
 const { Server } = require("socket.io");
+
 const io = new Server(http);
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
+
+     console.log('a user connected');
+
+
+     socket.on('new-user-joined', name =>{
+
+        console.log(`${name} just joined the chat`);
+        
+       users[socket.id]= name;
+       socket.broadcast.emit('user-joined',name)
+     });
+
+     socket.on('send', message=>{
+        socket.broadcast.emit('receive',{'message':message,'user':users[socket.id]})
+     });
+
+     console.log("everything goes well...");
+
   });
 
 
